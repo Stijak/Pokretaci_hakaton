52d816a7db913eafb99e26f1746b753b8c609153
Stijak <nemanja@stijak.com>
2014-05-16 20:21:48 +0200

    Zezanje sa FragmentManagerom

---
+++ src/rs/pokretaci/hakaton/MapActivity.java
@@ -0,0 +1,280 @@
+package rs.pokretaci.hakaton;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+
+import rs.pokretaci.hakaton.R;
+
+/**
+ * Activity that contains an interactive Google Map fragment. Users can record
+ * a traveled path, mark the map with information and take pictures that become
+ * associated with the map. 
+ */
+public class MapActivity extends Activity {
+	//drawer demo
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle mDrawerToggle;
+
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
+	private String[] mDrawwerList;
+
+	/** The interactive Google Map fragment. */
+	private GoogleMap m_vwMap;
+	private HashMap<Marker, String> mMarkerProblemIds = new HashMap<Marker, String>();
+
+
+	private MapFragment mapFragment;
+
+
+
+
+	/** Request codes for starting new Activities. */
+	private static final int ENABLE_GPS_REQUEST_CODE = 1;
+	private static final int PICTURE_REQUEST_CODE = 2;
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if (m_vwMap == null) {
+			m_vwMap = mapFragment.getMap();
+			m_vwMap.setMyLocationEnabled(true);
+		}
+
+	}
+
+	private class DownloadLocationsTask extends AsyncTask<Void, Void, JSONObject> {
+		@Override
+		protected JSONObject doInBackground(Void... voids) {
+			StringBuilder builder = new StringBuilder();
+			HttpClient client = new DefaultHttpClient();
+			HttpGet httpGet = new HttpGet("http://www.pokretaci.rs/api/goals/filter/trending?limit=50");
+			try {
+				HttpResponse response = client.execute(httpGet);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				if (statusCode == 200) {
+					HttpEntity entity = response.getEntity();
+					InputStream content = entity.getContent();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+					String line;
+					while ((line = reader.readLine()) != null) {
+						builder.append(line);
+					}
+				} else {
+					Log.e(MapActivity.class.toString(), "Failed to download file");
+				}
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				return new JSONObject(builder.toString());
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject object) {
+			JSONObject data;
+			try {
+				data = object.getJSONObject("data");
+				//String[] names = JSONObject.getNames(data);
+				JSONArray array = data.names();
+				for (int i = 0; i < array.length(); i++) {
+					String name = array.getString(i);
+					JSONObject problem = data.getJSONObject(name);
+					String title = problem.getString("title");
+					String description = problem.getString("description");
+					JSONObject geo = problem.getJSONObject("location").getJSONObject("geo");
+					double lat = geo.getDouble("lon");
+					double lon = geo.getDouble("lat");
+					LatLng location = new LatLng(lat, lon);
+					MarkerOptions mo = new MarkerOptions();
+					Marker marker = m_vwMap.addMarker(mo);
+					mMarkerProblemIds.put(marker, name);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initLocationData();
+		initLayout();
+		initDrawer();
+
+		new DownloadLocationsTask().execute();
+	}
+
+	/**
+	 * Initializes all Location-related data.
+	 */
+	private void initLocationData() {
+		// TODO
+	}
+
+	private void initDrawer() {
+		mTitle = mDrawerTitle = getTitle();
+		mDrawwerList = getResources().getStringArray(R.array.drawer_array);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// set a custom shadow that overlays the main content when the drawer opens
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+		// set up the drawer's list view with items and click listener
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, mDrawwerList));
+		//mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+		// enable ActionBar app icon to behave as action to toggle nav drawer
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		// ActionBarDrawerToggle ties together the the proper interactions
+		// between the sliding drawer and the action bar app icon
+		mDrawerToggle = new ActionBarDrawerToggle(
+				this,                  /* host Activity */
+				mDrawerLayout,         /* DrawerLayout object */
+				R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+				R.string.drawer_open,  /* "open drawer" description for accessibility */
+				R.string.drawer_close  /* "close drawer" description for accessibility */
+				) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+	}
+
+	/* Called whenever we call invalidateOptionsMenu() */
+	@Override
+	public boolean onPrepareOptionsMenu (android.view.Menu menu) {
+		// If the nav drawer is open, hide action items related to the content view
+		//boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		// menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu((android.view.Menu) menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// The action bar home/up action should open or close the drawer.
+		// ActionBarDrawerToggle will take care of this.
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle action buttons
+		/*switch(item.getItemId()) {
+        case R.id.action_websearch:
+            // create intent to perform web search for this planet
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
+            // catch event that there's no activity to handle intent
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
+            }
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }*/
+		return super.onOptionsItemSelected(item);
+	}
+
+
+	/**
+	 * Initializes all other data for the application.
+	 */
+	private void initLayout() {
+		setContentView(R.layout.map_layout);
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		GoogleMapOptions mapOptions = new GoogleMapOptions();
+		mapOptions.compassEnabled(true);
+		CameraPosition.Builder cameraBuilder = CameraPosition.builder();
+		LatLng belgrade = new LatLng(44.769010, 20.479202);
+		cameraBuilder.target(belgrade);
+		cameraBuilder.zoom(12);
+		mapOptions.camera(cameraBuilder.build());
+		mapFragment = MapFragment.newInstance(mapOptions);
+		fragmentTransaction.add(R.id.content_frame, mapFragment);
+		fragmentTransaction.commit();
+	}
+
+	/*@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = this.getSupportMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		return true;
+	}
+*/
+}
---
+++ src/rs/pokretaci/hakaton/ProblemDetailsActivity.java
@@ -0,0 +1,99 @@
+package rs.pokretaci.hakaton;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import rs.pokretaci.hakaton.R;
+
+public class ProblemDetailsActivity extends FragmentActivity {
+    ViewPager mViewPager;
+    SectionsPagerAdapter mTabsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.pager);
+        setContentView(R.layout.problem_details);
+
+        final ActionBar bar = getActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+
+        //mTabsAdapter = new TabsAdapter(this, mViewPager);
+        mTabsAdapter = new SectionsPagerAdapter(this.getSupportFragmentManager());
+        /*mTabsAdapter.addTab(bar.newTab().setText("Simple"),
+                CountingFragment.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText("List"),
+                FragmentPagerSupport.ArrayListFragment.class, null);
+        mTabsAdapter.addTab(bar.newTab().setText("Cursor"),
+                CursorFragment.class, null);*/
+
+        if (savedInstanceState != null) {
+            bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
+    }
+
+    /**
+     * This is a helper class that implements the management of tabs and all
+     * details of connecting a ViewPager with associated TabHost.  It relies on a
+     * trick.  Normally a tab host has a simple API for supplying a View or
+     * Intent that each tab will show.  This is not sufficient for switching
+     * between pages.  So instead we make the content part of the tab host
+     * 0dp high (it is not shown) and the TabsAdapter supplies its own dummy
+     * view to show as the tab content.  It listens to changes in tabs, and takes
+     * care of switch to the correct paged in the ViewPager whenever the selected
+     * tab changes.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			// getItem is called to instantiate the fragment for the given page.
+			// Return a PlaceholderFragment (defined as a static inner class
+			// below).
+			return null; //TODO return correspondingfragment
+		}
+
+		@Override
+		public int getCount() {
+			// Show 2 total pages.
+			return 2;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+			case 0:
+				return getString(R.string.page_details);
+			case 1:
+				return getString(R.string.page_comments);
+			}
+			return null;
+		}
+	}
+}
