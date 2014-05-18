3c701c41c3bae4c75354140811e1a208a69a8c6e
Stijak <nemanja@stijak.com>
2014-05-18 15:37:55 +0200

    Final hackaton commit

--- src/rs/pokretaci/hakaton/DetailsFragment.java
+++ src/rs/pokretaci/hakaton/DetailsFragment.java
@@ -15,6 +15,10 @@ public class DetailsFragment extends Fragment {
 	private TextView mTitle;
 	private TextView mDescription;
 	private ImageView mImage;
+	private ImageView mLocationImage;
+	private ImageView mAvatarImage;
+	private TextView mFullName;
+	private TextView mDate;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,6 +31,10 @@ public class DetailsFragment extends Fragment {
 		mTitle = (TextView) view.findViewById(R.id.details_title);
 		mDescription = (TextView) view.findViewById(R.id.details_content);
 		mImage = (ImageView) view.findViewById(R.id.image);
+		mLocationImage = (ImageView) view.findViewById(R.id.location_image);
+		mAvatarImage = (ImageView) view.findViewById(R.id.avatar_image);
+		mFullName = (TextView) view.findViewById(R.id.full_name);
+		mDate = (TextView) view.findViewById(R.id.details_date);
 		return view;
 	}
 	
@@ -35,6 +43,10 @@ public class DetailsFragment extends Fragment {
 		mTitle.setText(goal.title);
 		mDescription.setText(goal.description);
 		if (goal.image != null && !goal.image.equals("")) Picasso.with(getActivity()).load(goal.image).into(mImage);
+		if (goal.location_image != null && !goal.location_image.equals("")) Picasso.with(getActivity()).load(goal.location_image).resize(500, 200).centerCrop().into(mLocationImage);
+		if (goal.creator.avatar != null && !goal.creator.avatar.equals("")) Picasso.with(getActivity()).load(goal.creator.avatar).placeholder(R.drawable.avatar).into(mAvatarImage);
+		mFullName.setText(goal.creator.full_name);
+		mDate.setText(goal.created_at);
 	}
 
 }
